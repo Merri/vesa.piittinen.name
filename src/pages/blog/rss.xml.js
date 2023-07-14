622c8a86494c2c98646fe5b4140180e61966dbb7
Vesa Piittinen <vesa.piittinen@gmail.com>
2023-07-14 12:14:45 +0300

    Allow publishing articles on a later date

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,10 +1,15 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import isBefore from 'date-fns/isBefore'
 
 export async function get(context) {
+	const now = new Date()
+
 	return rss({
 		title: 'Blog by Vesa Piittinen',
 		description: 'A blog mostly dedicated around web development topics.',
 		site: context.site,
-		items: await pagesGlobToRssItems(import.meta.glob('./**/*.{md,mdx}')),
+		items: (await pagesGlobToRssItems(import.meta.glob('./**/*.{md,mdx}')))
+			.filter((item) => isBefore(item.pubDate, now))
+			.sort((a, b) => +a.pubDate - +b.pubDate),
 	})
 }
