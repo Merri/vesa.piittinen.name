d9bfb63c1de930ee1730e56c83f701f6098166b9
Vesa Piittinen <vesa.piittinen@gmail.com>
2025-02-15 13:33:23 +0200

    Switch over to Bun, remove unused deps

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-//import deno from '@astrojs/deno'
 import mdx from '@astrojs/mdx'
-import node from '@astrojs/node'
 import preact from '@astrojs/preact'
-import fs from 'node:fs'
-import { deflate } from 'pako'
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
