e40369c1d637df35c2d604b66100ac40be808e29
Vesa Piittinen <vesa.piittinen@gmail.com>
2025-02-15 06:27:20 +0200

    Use static build

--- astro.config.js
+++ astro.config.js
@@ -7,37 +7,8 @@ import preact from '@astrojs/preact'
 import fs from 'node:fs'
 import { deflate } from 'pako'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
-
-const isDeno = process.argv.includes('--deno')
-const isNode = !isDeno && process.argv.includes('--node')
-const isVercel = !isDeno && !isNode
-
-/** @type {import('vite').Plugin} */
-const uint8ArrayLoader = {
-	name: 'uint8Array-loader',
-	transform(_code, id) {
-		const [path, query] = id.split('?')
-		if (query !== 'uint8array') return null
-
-		const data = fs.readFileSync(path)
-		const hex = deflate(new Uint8Array(data), { level: 9 }).reduce(
-			(chars, number) => chars + number.toString(16).padStart(2, '0'),
-			''
-		)
-
-		return `import { inflate } from 'pako';
-const hex = '${hex}';
-const array = new Uint8Array(${hex.length / 2});
-for (let i = 0, j = 0; i < ${hex.length / 2}; i++, j+=2) {
-	array[i] = parseInt(hex.slice(j, j + 2), 16);
-}
-export default inflate(array);`
-	},
-}
 
 export default defineConfig({
-	adapter: /*(isDeno && deno()) ||*/ (isNode && node({ mode: 'standalone' })) || vercel(),
 	integrations: [
 		preact(),
 		mdx(),
@@ -45,13 +16,6 @@ export default defineConfig({
 			filter: (page) => !page.includes('rss.xml'),
 		}),
 	],
-	//output: 'static',
-	output: 'server',
+	output: 'static',
 	site: 'https://vesa.piittinen.name',
-	vite: {
-		optimizeDeps: {
-			exclude: ['postgres'],
-		},
-		plugins: [uint8ArrayLoader],
-	},
 })
